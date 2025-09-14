eeb568802a80b8ace3902677898d958bbf38358c
Yang <43675883+Yank3l@users.noreply.github.com>
2025-09-14 20:05:20 +0800

    Add files via upload

---
+++ ST-OT-MP-PSI/tools/benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Auto-generated script for n = 20, m = 10
+
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 0 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 1 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 2 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 3 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 4 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 5 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 6 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 7 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 8 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 9 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 10 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 11 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 12 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 13 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 14 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 15 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 16 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 17 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 18 &
+./bin/frontend.exe -n 20 -t 20 -m 10 -p 19 &
